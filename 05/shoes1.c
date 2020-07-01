d80df42497f525d24f013de75b4a078bc00bd4d3
Rance Campbell <rancewcampbell@gmail.com>
2020-06-30 20:31:24 -0600

    add shoes1 program

---
+++ 05/shoes1.c
@@ -0,0 +1,15 @@
+// shows1.c -- converts a show size to inches
+#include <stdio.h>
+#define ADJUST 7.31
+int main(void)
+{
+    const double SCALE = 0.333;
+    double shoe, foot;
+
+    shoe = 9.0;
+    foot = SCALE * shoe + ADJUST;
+    printf("Show size (men's)     foot length\n");
+    printf("%10.1f %15.2f inches\n", shoe, foot);
+
+    return 0;
+}
