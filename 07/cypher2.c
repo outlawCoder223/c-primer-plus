a8a0b82e581d3e7b44f70349f921f613bac4d782
Rance Campbell <rancewcampbell@gmail.com>
2020-07-13 06:56:06 -0600

    add cypher2 program

---
+++ 07/cypher2.c
@@ -0,0 +1,18 @@
+// cypher2.c -- alters input, preserving non-letters
+#include <stdio.h>
+#include <ctype.h>
+int main(void)
+{
+  char ch;
+
+  while ((ch = getchar()) != '\n')
+  {
+    if (isalpha(ch))
+      putchar(ch + 1);
+    else
+      putchar(ch);
+  }
+  putchar(ch);
+
+  return 0;
+}
