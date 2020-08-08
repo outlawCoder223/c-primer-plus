eaef8bdf16a656cd90df3ec9ecf75de435bfdfc5
Rance Campbell <rancewcampbell@gmail.com>
2020-08-08 07:23:53 -0600

    add ex 03 in chapter 15

--- 15/15ex03.c
+++ 15/15ex03.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
 #include <string.h>
+#include <limits.h>
 int findOnBits(char *bs);
+char *itobs(int n, char *ps);
 
 int main(void)
 {
-  char bs[9];
-  int onbits;
+  char bs[CHAR_BIT * sizeof(int) + 1];
+  int num, onbits;
   printf("Enter a binary number (8bit): ");
-  scanf("%s", bs);
+  scanf("%d", &num);
+  itobs(num, bs);
   onbits = findOnBits(bs);
   printf("There were %d bits on.\n", onbits);
   return 0;
@@ -20,4 +23,16 @@ int findOnBits(char *bs)
     if (bs[i] == '1')
       count++;
   return count;
+}
+
+char *itobs(int n, char *ps)
+{
+  int i;
+  const static int size = CHAR_BIT * sizeof(int);
+
+  for (i = size - 1; i >= 0; i--, n >>= 1)
+    ps[i] = (01 & n) + '0';
+  ps[size] = '\0';
+
+  return ps;
 }
